975d30e6505124fd30f8fcb646f441d4705546a5
GowrishMallipattana <gowrish.mallipattana@target.com>
2020-03-02 16:25:41 +0530

    updates

--- various01/basic/main.go
+++ various01/basic/main.go
@@ -3,9 +3,11 @@ package main
 import (
         //"fmt"
         "basic/pkg/functions"
+        "basic/internal/internalfunctions"
 )
 
 func main() {  
     functions.Hello()
+    internalfunctions.Hello2()
     //fmt.Println("Hello World")
 }
