f0291daa36e84487cb50700bbbb0a94d5a89332f
GowrishMallipattana <gowrish.mallipattana@target.com>
2020-03-18 20:02:48 +0530

    updates

---
+++ various01/randomstring01/main.go
@@ -0,0 +1,51 @@
+package main
+
+// https://www.calhoun.io/creating-random-strings-in-go/
+
+import (
+  "math/rand"
+  "time"
+  "fmt"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var seededRand *rand.Rand = rand.New(
+  rand.NewSource(time.Now().UnixNano()))
+
+func StringWithCharset(length int, charset string) string {
+  b := make([]byte, length)
+  for i := range b {
+    b[i] = charset[seededRand.Intn(len(charset))]
+  }
+  return string(b)
+}
+
+func String(length int) string {
+  return StringWithCharset(length, charset)
+}
+
+
+func CreateRandomString(length int) string {
+
+const charSet = "abcdefghijklmnopqrstuvwxyz" +
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var seedRand *rand.Rand = rand.New(
+  rand.NewSource(time.Now().UnixNano()))
+
+  b := make([]byte, length)
+  for i := range b {
+    b[i] = charSet[seedRand.Intn(len(charSet))]
+  }
+  return string(b)
+}
+
+func main() {
+
+fmt.Println(StringWithCharset(5, charset))
+fmt.Println(String(7))
+fmt.Println(CreateRandomString(9))
+
+}
