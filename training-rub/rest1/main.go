18aee77849545f7409503719fd5f1bf1251e5595
GowrishMallipattana <gowrish.mallipattana@target.com>
2020-04-06 16:34:50 +0530

    updates

--- training-rub/rest1/main.go
+++ training-rub/rest1/main.go
@@ -14,6 +14,10 @@ LastName  string
 EmpID     int
 }
 
+type EmployeeFirstName struct{
+   FirstName string `json:"name"`
+}
+
 func main(){
 
 e1 := Employee {
@@ -29,6 +33,8 @@ EmpID: 2,
 }
 
 employees := []Employee{e1,e2}
+//employeesFirstNames := []string{}
+employeesFirstNames := []EmployeeFirstName{}
 
 
 // create handlers
@@ -40,15 +46,32 @@ helloHandler := func(w http.ResponseWriter, r *http.Request) {
 employeesHandler := func(w http.ResponseWriter, r *http.Request) {
   log.Print("employees endpoing called")
   b,_ := json.Marshal(employees) //ignore err - so have "_" instead of err
+  w.Header().Set("Content-Type", "application/json")
+  io.WriteString(w,string(b))
+}
+
+employeesFirstNameHandler := func(w http.ResponseWriter, r *http.Request) {
+  log.Print("employees firstname endpoing called")
+
+
+  var efn EmployeeFirstName
+
+  for _, employee := range employees {
+      
+      efn = EmployeeFirstName{FirstName: employee.FirstName}
+      employeesFirstNames = append(employeesFirstNames, efn)
+  }
+  b,_ := json.Marshal(employeesFirstNames) //ignore err - so have "_" instead of err
+  w.Header().Set("Content-Type", "application/json")
   io.WriteString(w,string(b))
 }
 
 // register the handlers
 http.HandleFunc("/hello", helloHandler)
 http.HandleFunc("/employees", employeesHandler)
+http.HandleFunc("/employeefirstnames", employeesFirstNameHandler)
 
 // start the server
 log.Fatal(http.ListenAndServe(":8888", nil))
 
 }
-
