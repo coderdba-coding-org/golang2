1360287fd15fcb2c6daae5dce68db5b9558a9eaf
GowrishMallipattana <gowrish.mallipattana@target.com>
2020-05-19 16:37:47 +0530

    updates

---
+++ training-sri/version-inject/README.txt
@@ -0,0 +1,5 @@
+GO_LDFLAGS - to inject version number and such
+
+GO_LDFLAGS="-X somefile"
+
+https://www.digitalocean.com/community/tutorials/using-ldflags-to-set-version-information-for-go-applications
---
+++ various01/exec/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+        //"crypto/tls"
+        //"encoding/base64"
+        //"encoding/json"
+        //"flag"
+        "fmt"
+        //"www.github.com/coderdba-coding-org/exec/app/logger"
+        //"io/ioutil"
+        "log"
+        //"net"
+        //"net/http"
+        //"os"
+        "os/exec"
+        //"path"
+        "bytes"
+        //"strings"
+        //"time"
+)
+
+func main() {
+
+        log.Println("In main()")
+
+        runTestShellCommand1()
+        runTestShellCommand2()
+
+        // Maintenance loop
+        //for {
+                //log.Println("Loop check and fix")
+                ////fmt.Println("Loop check and fix")
+
+                //// Maintenance functions
+                //runTestKubectl1()
+                //patroniReinit()
+
+                //time.Sleep(300 * time.Second)
+        //}
+}
+
+
+func runTestShellCommand1() {
+        out, err := exec.Command("date").Output()
+        if err != nil {
+                log.Fatal(err)
+        }
+        // Printf prints string - println prints bytes
+        fmt.Println("===================")
+        fmt.Printf("The date Printf is %s\n", out)
+        fmt.Println("The date Println is %s\n", out) //println does not recognize formatter %s
+
+        fmt.Println("===================")
+
+        fmt.Printf("The date string(out) Printf is %s ", string(out))
+        fmt.Println("The date  string(out) Println is %s\n", string(out)) //println does not recognize formatter %s
+        fmt.Println("===================")
+}
+
+func runTestShellCommand2() {
+        //shellcmd := "ls"
+        shellcmd := "date"
+
+        cmd := exec.Command("bash", "-c", shellcmd)
+        var out bytes.Buffer
+        cmd.Stdout = &out
+        err := cmd.Run()
+        if err != nil {
+                log.Fatal(err)
+        }
+        fmt.Printf("Command output out.String() Printf: %q\n", out.String())
+        fmt.Println("Command output  out.String() Println: %q\n", out.String())
+
+}
+
+func runTestKubectl1() {
+
+        log.Println("In runTestKubectl1()")
+
+        cmd := fmt.Sprintf("kubectl get nodes")
+
+        fmt.Printf("Command is %s\n", cmd)
+        log.Print("Command is %s\n", cmd)
+
+        out, err := exec.Command("bash", "-c", cmd).Output()
+
+        if err != nil {
+                log.Println(err)
+                //log.Fatal(err)
+        }
+
+        fmt.Printf("Output is %s", out)
+        log.Printf("Output is %s\n", out)
+
+}
